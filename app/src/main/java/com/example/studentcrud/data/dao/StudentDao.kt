b4ca82942ecf1b6a40ae3eee8f83258502464149
said khamis <saidkhamis2306@gmail.com>
2026-01-26 10:13:22 +0300

    StudentDao.kt - Used to create the queries

--- app/src/main/java/com/example/studentcrud/data/dao/StudentDao.kt
+++ app/src/main/java/com/example/studentcrud/data/dao/StudentDao.kt
@@ -1,4 +1,20 @@
 package com.example.studentcrud.data.dao
 
-class StudentDao {
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import com.example.studentcrud.data.entity.Student
+
+@Dao
+interface StudentDao {
+    @Query("SELECT * FROM student")
+    fun getAllStudents(): Flow<List<Student>>
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudent(student: Student)
+
+    @Update
+    suspend fun updateStudent(student: Student)
+
+    @Delete
+    suspend fun deleteStudent(student: Student)
 }
