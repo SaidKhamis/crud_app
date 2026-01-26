9d468f36f5f5c597e849bc84c06fb51632aa53e4
said khamis <saidkhamis2306@gmail.com>
2026-01-26 10:29:57 +0300

    AppDatabase.kt - Used to create the database and ensure only one database is created over the project.

--- app/src/main/java/com/example/studentcrud/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/studentcrud/data/database/AppDatabase.kt
@@ -1,4 +1,36 @@
 package com.example.studentcrud.data.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.studentcrud.data.entity.Student
+import com.example.studentcrud.data.dao.StudentDao
+
+@Database (
+    Entities = (Student::class),
+    version = 1,
+    exportSchema = false
+)
+
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun studentDao(): StudentDao {
+
+        companion object {
+            @Volatile
+            private var INSTANCE: AppDatabase? = null
+
+            fun getDatabase(context: Context) : AppDatabase {
+                 return INSTANCE :? synchronized(this) {
+                     val instance = Room.databaseBuilder(
+                         context.applicationContext,
+                         AppDatabase::class.java,
+                         "student_db"
+                     ).build()
+                    INSTANCE = instance
+                    instance
+                }
+            }
+        }
+    }
 }
