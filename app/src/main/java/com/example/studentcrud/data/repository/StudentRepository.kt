73280027ae60394465d6dce1e30e7d70bf89bd62
SaidKhamis <saidkhamis2306@gmail.com>
2026-01-27 15:47:07 +0300

    StudentRepository.kt file inside the data package
    It used to link between the database and the UI.

--- app/src/main/java/com/example/studentcrud/data/repository/StudentRepository.kt
+++ app/src/main/java/com/example/studentcrud/data/repository/StudentRepository.kt
@@ -1,4 +1,16 @@
-package com.example.studentcrud.data.repository
+package com.example.studentcrudapp.data.repository
 
-class StudentRepository {
+import com.example.studentcrudapp.data.dao.StudentDao
+import com.example.studentcrudapp.data.entity.Student
+import kotlinx.coroutines.flow.Flow
+
+class StudentRepository(private val dao: StudentDao) {
+    val allStudents: Flow<List<Student>> = dao.getAllStudents()
+
+    suspend fun insert(student: Student) = dao.insertStudent(student)  
+
+    suspend fun update(student: Student) = dao.updateStudent(student)  
+
+    suspend fun delete(student: Student) = dao.deleteStudent(student)
+
 }
