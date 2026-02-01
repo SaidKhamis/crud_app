a8f11c6d758a338cf4df2cc8d0e68f810b67b3b1
said khamis <saidkhamis2306@gmail.com>
2026-02-01 22:08:16 +0300

    StudentListScreen.kt the file for holding the screen and UI of the APP

--- app/src/main/java/com/example/studentcrud/ui/screens/StudentListScreen.kt
+++ app/src/main/java/com/example/studentcrud/ui/screens/StudentListScreen.kt
@@ -1,2 +1,66 @@
 package com.example.studentcrud.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.studentcrud.data.entity.Student
+import com.example.studentcrud.viewmodel.StudentViewModel
+
+@Composable
+fun StudentListScreen(viewModel: StudentViewModel) {
+    val students by viewModel.students.collectAsState()
+
+    var name by remember { mutableStateOf("") }
+    var course by remember {mutableStateOf("")}
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        TextField(
+            value = name,
+            onValuechange = {name = it},
+            label = {Text("Name")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = course,
+            onValuechange = {course = it},
+            label = {Text("Course")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button (
+           onClick = {
+               if(name.isNotBlank() && course.isNotBlank()){
+                   viewModel.addStudent (
+                       Student(name = name, course = course)
+                   )
+                   name = ""
+                   course = ""
+               }
+           }, modifier = Modifier.fillMaxWidth()
+        ){
+            Text("Add Student")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn {
+            items(students) { student ->
+                Text(
+                    text = "${student.name} - ${student.course}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
